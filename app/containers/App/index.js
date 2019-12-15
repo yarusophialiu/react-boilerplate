17c992b2ded2b819c9800a02d3688afaf72018a8
yaruliu <yarusophia@gmail.com>
2019-12-15 11:55:08 -0500

    create User page

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,6 +14,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Users from 'containers/UsersPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
@@ -41,6 +42,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
+        <Route path="/users" component={Users} />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
