2ef3428c7abaff95b2e58a84e2c7b23d47ec032f
yaruliu <yarusophia@gmail.com>
2019-12-15 20:04:27 -0500

    add user action

--- app/containers/UsersPage/actions.js
+++ app/containers/UsersPage/actions.js
@@ -1,7 +1,12 @@
-import { GET_USERS_REQUEST } from './constants';
+import { GET_USERS_REQUEST, GET_USERS_SUCCESS } from './constants';
 
 export function getUsers() {
   return {
     type: GET_USERS_REQUEST,
   };
 }
+
+export const getUsersSuccess = ({ items }) => ({
+  type: GET_USERS_SUCCESS,
+  payload: { items },
+});
--- app/containers/UsersPage/constants.js
+++ app/containers/UsersPage/constants.js
@@ -5,3 +5,4 @@
  */
 
 export const GET_USERS_REQUEST = 'boilerplate/User/GET_USERS_REQUEST';
+export const GET_USERS_SUCCESS = 'boilerplate/User/get_users_success';
